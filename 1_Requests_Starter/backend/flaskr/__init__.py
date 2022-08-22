521ce48112f30611ac7f3ad5859f08b24f1f60a4
Wandji69 <collinschuwa@gmail.com>
2022-08-22 20:10:10 +0100

    Error handling and Unit test

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -150,4 +150,43 @@ def create_app(test_config=None):
         except:
             abort(422)
 
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+        "success": False, 
+        "error": 422,
+        "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(404)
+    def unprocessable(error):
+        return jsonify({
+        "success": False, 
+        "error": 404,
+        "message": "Resource Not Found"
+        }), 404
+    
+    @app.errorhandler(400)
+    def unprocessable(error):
+        return jsonify({
+        "success": False, 
+        "error": 400,
+        "message": "Bad Request"
+        }), 400
+
+    @app.errorhandler(405)
+    def unprocessable(error):
+        return jsonify({
+        "success": False, 
+        "error": 405,
+        "message": "Method not allowed"
+        }), 405
+
+    @app.errorhandler(500)
+    def unprocessable(error):
+        return jsonify({
+        "success": False, 
+        "error": 500,
+        "message": "Resource Not Found"
+        }), 500
     return app
